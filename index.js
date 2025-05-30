f017e310560fca55ccdbeb9b92def00341e790ee
stephcra123 <186585465+stephcra123@users.noreply.github.com>
2025-05-29 22:27:38 -0400

    Added image and description

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const artwork_num = Math.round(Math.random()*100000/12) // get a page number
 document.addEventListener('DOMContentLoaded', function() {
-fetch(`https://api.artic.edu/api/v1/artworks/?fields=title,artist,artist_display,description,id&limit=12&page=${artwork_num}`) 
+fetch(`https://api.artic.edu/api/v1/artworks/?fields=image_id,title,artist,artist_display,description,id&limit=1&page=${artwork_num}&has_image=true`) 
 .then(response => {
     if (!response.ok) {
       throw new Error('Request failed');  
@@ -13,21 +13,50 @@ fetch(`https://api.artic.edu/api/v1/artworks/?fields=title,artist,artist_display
   })
   .then(data => {
     const artworks = data.data;
-    console.log('number of peices: ',artworks.length)
-    const artSection = document.getElementById('data-container');
-    const artList = artSection.querySelector('ul');
-  
-    for (let i = 0; i < artworks.length; i++) {
-      const artwork = document.createElement('li');
-      artwork.textContent = artworks[i].title;
-      console.log(i)
-      //artwork.className = 'projects-item'; // Add class for styling
-      artList.appendChild(artwork);
-    }
-      return artworks
-    })
+    const image = document.getElementById('image')
+    const art_titles = document.getElementById('art_title')
+    const artworkData = artworks[0];
+    const artwork = document.createElement('li');
+    //title
+    art_titles.textContent = artworkData.title;
+    console.log(art_titles)
+    //image
+    artwork.image_id = artworkData.image_id;
+    artwork.imageUrl = `https://www.artic.edu/iiif/2/${artworkData.image_id}/full/843,/0/default.jpg`;
+    image.src = artwork.imageUrl
+    console.log(image)
+
+    return artwork
+  })
   .catch(error => {
     console.error('Error fetching data:', error);
     document.getElementById('data-container').innerHTML = "<p>Error loading data. Please try again.</p>";
   });
 });
+console.log('Image element found:', image);
+console.log('Image element type:', typeof image);
+
+fetch(`https://api.artic.edu/api/v1/artworks/?fields=description&limit=1&page=${artwork_num}&has_image=true`) 
+.then(response => {
+    if (!response.ok) {
+      throw new Error('Request failed');  
+    }
+    //console.log(response.json())
+    return response.json(); // Parse the response as JSON
+    
+  })
+  .then(data => {
+    const artworks = data.data;
+    const artworkData = artworks[0];
+    const descriptions = document.getElementById('descript')
+    descriptions.textContent = artworkData.description
+    console.log(artworkData.description)
+    console.log(descriptions.textContent)
+  
+
+    return artworks
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+    document.getElementById('data-container').innerHTML = "<p>Error loading data. Please try again.</p>";
+  });
